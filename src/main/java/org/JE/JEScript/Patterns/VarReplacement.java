77e2bf3faf3d2411842f4d9a5ca4397cba71f61a
NoahFreelove <90488068+NoahFreelove@users.noreply.github.com>
2023-03-03 23:00:20 -0500

    Added more auto-imports

--- src/main/java/org/JE/JEScript/Patterns/VarReplacement.java
+++ src/main/java/org/JE/JEScript/Patterns/VarReplacement.java
@@ -8,14 +8,20 @@ public class VarReplacement {
 
     static {
         replacements.put("vec3", new ReplacementResult("Vector3f","import org.joml.Vector3f;"));
+        replacements.put("Vector3f", new ReplacementResult("Vector3f","import org.joml.Vector3f;"));
         replacements.put("vec3i", new ReplacementResult("Vector3i","import org.joml.Vector3i;"));
+        replacements.put("Vector3i", new ReplacementResult("Vector3i","import org.joml.Vector3i;"));
         replacements.put("vec2", new ReplacementResult("Vector2f","import org.joml.Vector2f;"));
+        replacements.put("Vector2f", new ReplacementResult("Vector2f","import org.joml.Vector2f;"));
         replacements.put("vec2i", new ReplacementResult("Vector2i","import org.joml.Vector2i;"));
+        replacements.put("Vector2i", new ReplacementResult("Vector2i","import org.joml.Vector2i;"));
+
         replacements.put("bool ", new ReplacementResult("boolean ",""));
         replacements.put("string", new ReplacementResult("String",""));
         replacements.put("Str", new ReplacementResult("String",""));
         replacements.put("GO", new ReplacementResult("GameObject","import org.JE.JE2.Objects.GameObject;"));
         replacements.put("gameobject", new ReplacementResult("GameObject","import org.JE.JE2.Objects.GameObject;"));
+        replacements.put("GameObject", new ReplacementResult("GameObject","import org.JE.JE2.Objects.GameObject;"));
         replacements.put("cam", new ReplacementResult("Camera","import org.JE.JE2.Rendering.Camera;"));
         replacements.put("run", new ReplacementResult("()->",""));
         replacements.put("lam", new ReplacementResult("this::",""));
